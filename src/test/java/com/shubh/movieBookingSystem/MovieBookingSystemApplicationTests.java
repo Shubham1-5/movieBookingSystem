fea19be0a38f0ff72bd152fabed55054e870af9b
Shubham Singh <singh.shbhm90@gmail.com>
2021-09-03 18:31:38 +0530

    Initial commit

---
+++ src/test/java/com/shubh/movieBookingSystem/MovieBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubh.movieBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
