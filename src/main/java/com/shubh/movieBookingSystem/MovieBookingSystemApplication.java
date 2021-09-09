c68cde65bd58479588e3bc0586e2e43c73f2a5c7
Shubham Singh <singh.shbhm90@gmail.com>
2021-09-09 18:54:41 +0530

    logging by Log4j2 framework and saved in ext file

--- src/main/java/com/shubh/movieBookingSystem/MovieBookingSystemApplication.java
+++ src/main/java/com/shubh/movieBookingSystem/MovieBookingSystemApplication.java
@@ -4,19 +4,34 @@ import com.shubh.movieBookingSystem.dao.MovieDao;
 import com.shubh.movieBookingSystem.entities.Movie;
 import com.shubh.movieBookingSystem.services.InitService;
 import com.shubh.movieBookingSystem.services.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class MovieBookingSystemApplication {
+
+	/**
+	 * We need the logger object
+	 * @param args
+	 */
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MovieBookingSystemApplication.class);
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(MovieBookingSystemApplication.class, args);
 
 		System.out.println("Hello Spring Boot !!! ");
+
+		LOGGER.debug("Writing for debug");
+		LOGGER.info("Writing for info");
+		LOGGER.warn("Writing for warn");
+		LOGGER.error("Writing for error");
 	}
 	@Bean
 	CommandLineRunner init (InitService initService){
